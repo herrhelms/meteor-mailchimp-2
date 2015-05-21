a63ad22db6f0cfc8b77afa127403f05cf18d6de5
Herrhelms <dev@straw.io>
2015-05-21 01:54:36 +0000

    give miro credit

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'herrhelms:meteor-mailchimp-2',
     version: '0.0.1',
-    summary: 'MailChimp API 2.0 ported to meteor (with custom signup form)',
+    summary: 'MailChimp API 2.0 with custom subscribe form, based on miro:mailchimp',
     git: 'https://github.com/herrhelms/meteor-mailchimp-2.git',
     documentation: 'README.md'
 });
